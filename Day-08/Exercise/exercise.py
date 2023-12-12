06e7f812bb0d96339e234a804365838f147b7c80
Sreenivas09 <87257742+Sreenivas09@users.noreply.github.com>
2023-12-12 09:45:42 +0000

    adding exercise py

---
+++ Day-08/Exercise/exercise.py
@@ -0,0 +1,14 @@
+
+Empy_list = ["Chris", "Jack", "Ben", "Marta", "Michl"]
+Empy_list.append("Sreeni")
+Index_Element = Empy_list[0:5]
+list_length = len(Empy_list)
+new_list = Empy_list + [5, 6]
+Empy_list.sort()
+is_present = 'Non' in Empy_list
+
+print(Empy_list)
+print(Index_Element)
+print(list_length)
+print(new_list)
+print(is_present)
